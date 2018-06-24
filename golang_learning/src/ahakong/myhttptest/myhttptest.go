50089592285bc2503ffb2498677338eca4e36ae2
ahakong <ahakong@sina.com>
2018-06-24 20:06:50 +0800

    http 中文html无法渲染出页面问题

---
+++ golang_learning/src/ahakong/myhttptest/myhttptest.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"fmt"
+)
+
+func login(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	if method == "GET" {
+		t, err := template.ParseFiles("./login2.html")
+		if err != nil {
+			fmt.Fprintf(w, "load login.html failed")
+			return
+		}
+		t.Execute(w, nil)
+	} else if method == "POST" {
+		r.ParseForm()
+		username := r.FormValue("username")
+		passwd := r.FormValue("passwd")
+		fmt.Printf("username:%s\n", username)
+		fmt.Printf("passwd:%s\n", passwd)	
+		if username == "admin" && passwd == "admin123" {
+			fmt.Fprintf(w, "user %s login success", username)
+		} else {
+			fmt.Fprintf(w, "user %s login failed", username)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Printf("listen server failed,err:%v\n", err)
+		return
+	}
+}
+
